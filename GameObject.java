b48afe3af7c4b95ba23a3a9f87698c4af7c8b5f4
Shanmukh1226 <shanmukharavapalli@gmail.com>
2025-05-13 10:50:12 -0400

    Update GameObject.java

--- GameObject.java
+++ GameObject.java
@@ -1,7 +1,34 @@
+import javax.swing.JLabel;
+
 public abstract class GameObject extends JLabel{
     public GameObject(int x, int y){
         this.setLocation(x,y);
     }
 
   public abstract void update();
+
+ public boolean hasCollidedWith(GameObject other) {
+        // TODO implement collision detection here
+        if(this.getX() <= other.getX() + other.getWidth()){//left of hero right of obj
+            return true;
+        }
+        if(this.getX() + this.getWidth() >= other.getX() ){//right of hero left of obj
+            return true;
+        }
+        if(this.getY() <= other.getY() + other.getHeight()){//up of hero down of obj
+            return true;
+        }
+        if(this.getY() + this.getHeight() >= other.getY() ){//down of hero up of obj
+            return true;
+        }
+
+
+
+
+        return false;
+    }
+
+
+
+    
 }
