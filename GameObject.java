0229e9a61fb74f2a24d72ae657e13d76c06db9bc
iloveCSA <karsten_blum@icloud.com>
2025-05-23 17:44:50 -0400

    Update GameObject.java

--- GameObject.java
+++ GameObject.java
@@ -1,11 +1,32 @@
 import javax.swing.JLabel;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
 public abstract class GameObject extends JLabel{
-    public GameObject(int x, int y){
+    protected boolean interactable;
+    protected BufferedImage image;
+    protected int x;
+    protected int y;
+    protected boolean isVisible;
+
+    public GameObject(int x, int y, boolean interactable, boolean isVisible){
+        this.interactable = interactable;
+        this.isVisible = isVisible;
+        this.x = x;
+        this. y = y;
         this.setLocation(x,y);
+        
     }
 
   public abstract void update();
+  public abstract void interact();
+
+  public void render(Graphics g){
+        if(isVisible && image != null){
+            g.drawImage(image,x,y, null);
+        }
+  }
 
  public boolean hasCollidedWith(GameObject other) {
         // TODO implement collision detection here
@@ -21,12 +42,15 @@ public abstract class GameObject extends JLabel{
         if(this.getY() + this.getHeight() >= other.getY() ){//down of hero up of obj
             return true;
         }
-
-
-
-
         return false;
     }
+
+public boolean getInteractable(){
+    return interactable;
+}
+public boolean getVisibility(){
+    return isVisible;
+}
 
 
 
