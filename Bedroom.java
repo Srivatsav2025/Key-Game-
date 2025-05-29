8b3cf6c07ef674350d5afa970aab4c90f4e3eb0e
iloveCSA <karsten_blum@icloud.com>
2025-05-29 08:02:30 -0400

    Update Bedroom.java

--- Bedroom.java
+++ Bedroom.java
@@ -7,18 +7,22 @@ public class Bedroom extends Room{
     private Player player;
     private Couch couch;
     private Couch interactableCouch;
+    private Bed bed;
 
     public Bedroom(Player player){
         this.player = player;
         bedroomKey = new Key(300,300,player);
-        bedroomDoor = new Door(400,100, bedroomKey, player);
+        bedroomDoor = new Door(400,50, bedroomKey, player);
         couch = new Couch(200,400,false,this);
-        interactableCouch = new Couch(400,400,true,this);
+        interactableCouch = new Couch(500,400,true,this);
+        bed = new Bed(1000,200,false,this);
+
         objects = new ArrayList<GameObject>();
         objects.add(bedroomKey);
         objects.add(bedroomDoor);
         objects.add(couch);
         objects.add(interactableCouch);
+        objects.add(bed);
     }
 
     
@@ -34,11 +38,11 @@ public class Bedroom extends Room{
         return objects;
     }
 
-    public Player getPlayer(){
-        return player;
-    }
     public Key getKey(){
         return bedroomKey;
+    }
+    public Door getDoor(){
+        return bedroomDoor;
     }
     public GameObject removeKey(){
         objects.remove(bedroomKey);
